79645eb27880d536e71deeabb90ced0e737f0bf8
tubi55 <tubi55@nate.com>
2022-10-13 17:35:45 +0900

    layout.js에 useRef와 useEffect를 이용하여 서브페이지 전환효과 부여

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,6 +1,13 @@
+import { useRef, useEffect } from 'react';
 function Layout(props) {
+    const frame = useRef(null);
+
+    useEffect(() => {
+        frame.current.classList.add("on");
+    }, []);
+
     return (
-        <section className={`content ${props.name}`}>
+        <section className={`content ${props.name}`} ref={frame}>
             <figure></figure>
             <div className="inner">
                 <h1>{props.name}</h1>
@@ -10,3 +17,22 @@ function Layout(props) {
     );
 }
 export default Layout;
+
+/*
+useEffect
+
+- 컴포넌트가 마운트 되었을때
+=> 처음나타났을때 <---
+=> props로 받은 값을 컴포넌트의 로컬상태로 설정할때
+=> 외부  API요청이 있을때 
+=> setIntercal, setTimeout 통해 작업이 예약될때
+
+- 컴포넌트가 언마운트 되었을때
+=> 사라질때 
+=> setIntercal, setTimeout을 사용하여 등록한 작업들이 클리어 되었을때
+=> 라이브러리 인스턴스가 제거됬을때
+
+- 컴포넌트가 업데이트될때
+=> 특정 props가 바뀔때
+
+*/
