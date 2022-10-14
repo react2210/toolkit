09e4d14032e1664469093dec80ee84f297520f99
tubi55 <tubi55@nate.com>
2022-10-14 16:19:36 +0900

    youtube 스크롤생기는 문제 해결, 클린업함수

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 function Popup(props) {
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "auto";
+        }
+    }, []);
+
+
     return (
         <aside className="pop">
             <div className="con">{props.children}</div>
