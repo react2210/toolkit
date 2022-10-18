d80ec53cb820d2bf5ea876656ee4e74bb2fbfbd0
tubi55 <tubi55@nate.com>
2022-10-18 11:11:27 +0900

    기본로직 주석으로 순서에 따라 정리

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -3,11 +3,11 @@ import Layout from '../common/Layout';
 
 function Member() {
 
-    //userid 의 입력값이 담길 초기 state를 객체로 지정
+
     const initVal = {
         userid: '',
     };
-    //해당 객체값을 state에 초기값으로 저장
+
     const [Val, setVal] = useState(initVal);
 
     const [Err, setErr] = useState({});
@@ -21,32 +21,27 @@ function Member() {
         return errs;
     };
 
-    /*
-    대입형 함수의 특징 : 함수 표현식이라고 하는데 이 함수의 특징은 호이스팅에 영향을 받지 않는다
-    콜백으로 사용이 가능 ( 다른 함수의 인자로 넘길수 있음, 순서에 따라서 적용됨)
-    클로저로 사용이 가능 - 클로저는 매우 어렵지만 자바스크립트의 중요한 개념
-    클로저란 함수자신이 만들어진 환경(scope)을 기억해서 외부에서 호출되거나, 상위함수가 종료되더라도 해당 환경을 기억해서 접근할 수 있는 함수를 말한다 즉 메모리상에서 환경을 기억한다는 점에서 메모리를 점유하고 있어 메모리상으론 손해다 하지만 자바스크립의 강력한 기능으로 적극적으로 사용된다
-    */
     const handleChange = (e) => {
+        //순서 2 - 입력하고 있는 인풋요소의 네임, 벨류값을 변수로 비구조화 할당
         const { name, value } = e.target;
-        //es5에서는 객체에서 키값을 변수로 지정할 수 없었다
-        //es6에서 객체의 키값을 변수로 치환하고자 한다면 키 변수명을 []로 감싸준다
-        //[name]  === 'userid'
-        // setVal({ ...Val, userid: e.target.value });
+        //순서 3 - 비구조화 할당으로 받은 값을 Val state에 저장하고 
+        //순서 4 -setVal함수가 렌더링해서 우리가 볼 수 있도록 함
         setVal({ ...Val, [name]: value });
     }
 
     const handleSubmit = (e) => {
-        //일단 서버전송, 이동을 막아줘야한다 a태그가 아니어도 submit자체가 가지는 속성을 막는다
+        //순서 6 일단 서버전송은 막아준다
         e.preventDefault();
-        //check함수에 인수로 Val값을 넣어서 
+        //순서 7 Val state값을 인수로 전달해서 check함수에서 인증검사 시작 <={ check(Val)}
         setErr(check(Val));
+        //순서 8 인증검사 결과 errs가 존재한다면 반환된 에러 객체를 Err state에 옮겨 담음
     };
 
 
     return (
         <Layout name={'Member'}>
-            <form action="">
+            {/* 순서 5 전송버튼 클릭시 핸들서브밋함수를 호출 */}
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>회원가입 폼 양식</legend>
                     <table border='1'>
@@ -64,9 +59,10 @@ function Member() {
                                         id='userid'
 
                                         value={Val.userid}
+                                        //순서1 - 인풋에 값을 입력시 핸들체인지 함수가 호출
                                         onChange={(e) => { handleChange }}
                                     />
-                                    {/* 혹시 에러가 있으면 Err정보값을 화면에 출력 */}
+                                    {/* 순서 9 만약 에러객체가 있다면 화면에 출력 */}
                                     <span className='err'>{Err.userid}</span>
                                 </td>
                             </tr>
