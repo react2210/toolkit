ab5cf88e5efbd5a1844033d52b39d0669ed7729c
tubi55 <tubi55@nate.com>
2022-10-19 09:28:23 +0900

    글저장 기능 추가

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,12 +2,29 @@ import { useEffect, useRef, useState } from "react";
 import Layout from "../common/Layout";
 export default function Community() {
 
+
+
     const input = useRef(null);
     const textarea = useRef(null);
     const [Posts, setPosts] = useState([]);
 
+
+    const resetForm = () => {
+        input.current.value = '';
+        textarea.current.value = '';
+    }
+
     //글저장 함수
-    const createPost = () => { };
+    const createPost = () => {
+        setPosts([
+            ...Posts,
+            {
+                title: input.current.value,
+                content: textarea.current.value
+            },
+        ]);
+        resetForm();
+    };
 
     //Posts의 값이 변경될때마다 콘솔출력해서 우리가 볼 수 있는 
     useEffect(() => {
@@ -21,7 +38,7 @@ export default function Community() {
                 <br />
                 <textarea cols="30" rows="5" placeholder="본문을 입력하세요" ref={textarea}></textarea>
                 <br />
-                <button>CANCLE</button>
+                <button onClick={resetForm}>CANCLE</button>
                 <button onClick={createPost}>WRITE</button>
             </div>
         </Layout>
