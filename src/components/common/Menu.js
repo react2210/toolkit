e960bff8c2458927e65106c6da4196b7d4279e63
tubi55 <tubi55@nate.com>
2022-10-26 11:21:26 +0900

    menu컴포넌트 추가설정

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -3,52 +3,58 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { Link, NavLink } from 'react-router-dom';
 
 function Menu() {
-    const active = { color: 'orange' };
+	const [Open, setOpen] = useState(true);
+	const active = { color: 'orange' };
 
-    return (
-        <nav>
-            <h1>
-                <Link to='/'>
-                    <img src={process.env.PUBLIC_URL + '/img/logo_w.png'} alt="logo" />
-                </Link>
-            </h1>
+	return (
+		<AnimatePresence>
+			{Open && (
+				<nav>
+					<h1>
+						<Link to='/'>
+							<img src={process.env.PUBLIC_URL + '/img/logo_w.png'} alt="logo" />
+						</Link>
+					</h1>
 
-            <ul id="gnb">
-                <li>
-                    <NavLink to='/department' activeStyle={active}>
-                        Department
-                    </NavLink>
+					<ul id="gnb">
+						<li>
+							<NavLink to='/department' activeStyle={active}>
+								Department
+							</NavLink>
 
-                </li>
-                <li>
-                    <NavLink to='/community' activeStyle={active}>
-                        Community
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/gallery' activeStyle={active}>
-                        Gallery
-                    </NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeStyle={active}>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeStyle={active}>
+								Gallery
+							</NavLink>
 
-                </li>
-                <li>
-                    <NavLink to='/youtube' activeStyle={active}>
-                        Youtube
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/location' activeStyle={active}>
-                        Location
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/member' activeStyle={active}>
-                        Member
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+						</li>
+						<li>
+							<NavLink to='/youtube' activeStyle={active}>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/location' activeStyle={active}>
+								Location
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/member' activeStyle={active}>
+								Member
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+			)}
+		</AnimatePresence>
+
+	);
 }
 
 export default Menu;
