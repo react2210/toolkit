1e4a7add1869024da58e9ec8c6c8494e14f02c29
tubi55 <tubi55@nate.com>
2022-10-26 14:32:19 +0900

    서브컴포넌트에 서브 비주얼 출력

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -8,7 +8,9 @@ function Layout(props) {
 
     return (
         <section className={`content ${props.name}`} ref={frame}>
-            <figure></figure>
+            <figure>
+                <img src={`${process.env.PUBLIC_URL}/img/${props.name}.jpg`} alt={props.name} />
+            </figure>
             <div className="inner">
                 <h1>{props.name}</h1>
                 {props.children}
