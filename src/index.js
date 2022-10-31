daa06743d697425b3fc8f294054af89b2ec152cc
tubi55 <tubi55@nate.com>
2022-10-31 15:39:46 +0900

    index.js에서 store를 루트컴포넌트 App에 전달

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import youtubeReducer from './redux/youtubeSlice';
+
+const store = configureStore({
+	reducer: {
+		youtube: youtubeReducer
+	}
+})
 
 ReactDOM.render(
 	<React.StrictMode>
 		<HashRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
