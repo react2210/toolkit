bd9e8189dcbe431b35c793be21221f84516480fa
tubi55 <tubi55@nate.com>
2022-10-31 15:45:50 +0900

    App컴포넌트 마운트시 slice파일로부터 fetchYoutube함수 불러와서 호출 뒤,결과값을 dsipatch로 전달

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
+import { fetchYoutube } from './redux/youtubeSlice';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 //common
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 
-
-
 //main
 import Main from './components/main/Main';
-
 
 //sub
 import Community from './components/sub/Community';
@@ -20,6 +20,11 @@ import Youtube from './components/sub/Youtube';
 
 import './scss/style.scss';
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchYoutube());
+	}, [])
 	return (
 		<>
 			{/* Switch는 같은 경로의 라우터 연결시 구체적인 라우터 하나만 적용한다 */}
