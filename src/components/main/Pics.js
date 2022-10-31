329755fb043f2feef05963d5a86f3e5c7211d137
tubi55 <tubi55@nate.com>
2022-10-31 17:17:51 +0900

    flickr 리덕스툴깃으로 메인 컴포넌트 미리보기 출력

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,26 +1,31 @@
+import { useSelector } from 'react-redux';
+
 function Pics({ Scrolled, start }) {
-    //변수 = 특정값 || 대체값;
-    //변수에 대입되는 특정값이 undefined, NaN 같이 비정상인 값이 들어올때 대신 적용될 대체값을 설정해주는것
+    const Pics = useSelector(store => store.flickr.data);
     const position = Scrolled - start || 0;
-    //position => 전체 스크롤 거리값에서 해당 섹션요소의 세로 위치값을 뺀것으로 , 처음섹션의 초입에는 0이된다
-    console.log(position);
-    // console.log(Scrolled);
     return (
         <main id="pics" className='myScroll'>
             <p
                 style={{
                     left: 100 + position,
                 }}
-            // style={
-            //     position >= 0
-            //         ? { left: 100 + position, }
-            //         : null
             >FLICKR</p>
             <h3
                 style={{
                     left: 100 + position / 2,
                 }}
             >FLICKR</h3>
+
+            <ul>
+                {Pics.map((pic, idx) => {
+                    if (idx >= 4) return;
+                    return (
+                        <li key={idx}>
+                            <img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} />
+                        </li>
+                    )
+                })}
+            </ul>
         </main>
     );
 }
